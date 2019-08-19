8ecb877d072915e264367db7960b6153817f79fc
sansarip <pehrans@gmail.com>
2019-08-19 00:34:38 -0400

    added summary component

--- src/cljs/my_website/cards/components.cljs
+++ src/cljs/my_website/cards/components.cljs
@@ -1,12 +1,35 @@
 (ns my-website.cards.components
   (:require [devcards.core :refer-macros (defcard)]
             [sablono.core :as sab]
-            [my-website.styles :refer [components]]))
+            [reagent.core :refer [as-element]]
+            [my-website.styles :refer [color-palette]]
+            [my-website.components.summary :refer [summary]]))
 
 (defcard
-  hello-world
-  "Title description component"
-  (fn [] (sab/html [:div {:class-name (:container components)}
-                    [:h1 "Hello, World!"]
-                    [:h2 "Second heading"]])))
+  summary
+  "Basic summary component"
+  (fn [] (sab/html (as-element [:div {:style {:display "flex"
+                                              :justify-content "space-between"
+                                              :background-color (:primary color-palette)}}
+                                [:> summary {:header "Scratch My Patch"
+                                             :inverse true}
+                                 "Satisfy your primal urge to scritch that infernal itch!"]]))))
+(defcard
+  summaries
+  "Group of summary components"
+  (fn [] (sab/html (as-element [:div {:style {:display "flex"
+                                              :justify-content "space-between"
+                                              :flex-wrap "wrap"
+                                              :background-color (:primary color-palette)}}
+                                [:> summary {:header "Tootleoo"
+                                             :inverse true}
+                                 "Can you defeat the meanie poots with your magical toots?"]
+                                [:> summary {:header  "Scratch My Patch"
+                                             :inverse true}
+                                 "Satisfy your primal urge to scritch that infernal itch!"]
+                                [:> summary {:header  "Kiss My Piss"
+                                             :inverse true}
+                                 "Approved by the man, the myth, the legend—Richard Hendricks."]]))))
+
+
 
---
+++ src/cljs/my_website/components/summary.cljs
@@ -0,0 +1,43 @@
+(ns my-website.components.summary
+  (:require [reagent.core :as r]
+            [my-website.utilities :refer [word-concat]]
+            [my-website.styles :refer [color-palette]]
+            [cljs-css-modules.macro :refer-macros [defstyle]]))
+
+(defstyle summary-style
+          [".container" {:padding "1em"}]
+          [".inverse" {:color "white"}]
+          [".separator" {:background-color (:tertiary color-palette)
+                         :width            "7em"
+                         :height           ".5em"
+                         :margin-top       "1em"
+                         :margin-bottom    "1em"
+                         :transition       "width 2s"}]
+          [".header" {:font-family "\"Lucida Console\", Monaco, monospace"
+                      :margin      "0"
+                      :white-space "nowrap"}]
+          [".container:hover > div[name=separator]" {:width "10em"}]
+          [".description" {:font-family "\"Lucida Console\", Monaco, monospace"
+                           :width       "60%"
+                           :height      "auto"}])
+
+(def summary
+  (r/create-class {:render (fn [this] (let [inverse (-> this .-props .-inverse)
+                                            classes (-> this .-props .-extraClasses)
+                                            style (-> this .-props .-style)
+                                            children (-> this .-props .-children)
+                                            header (-> this .-props .-header)
+                                            description (-> this .-props .-description)]
+                                        [:div {:class (word-concat
+                                                        (:container summary-style)
+                                                        (if inverse (:inverse summary-style))
+                                                        classes)
+                                               :style style}
+                                         [:h1 {:class (:header summary-style)} header]
+                                         [:div {:class (:separator summary-style)
+                                                :name  "separator"}]
+                                         [:div {:class (:description summary-style)}
+                                          description
+                                          children]]))}))
+
+
