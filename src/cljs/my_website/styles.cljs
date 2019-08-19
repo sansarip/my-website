aa1a8763af32bbb332e2cded39cbd4f49e9f0b51
sansarip <pehrans@gmail.com>
2019-08-19 00:34:10 -0400

    added color palette

--- src/cljs/my_website/styles.cljs
+++ src/cljs/my_website/styles.cljs
@@ -1,6 +1,7 @@
 (ns my-website.styles
   (:require [cljs-css-modules.macro :refer-macros [defstyle]]))
 
-(defstyle components
-  [".container" {:background-color "blue"
-                 :font-size 55}])
+(def color-palette {:primary "#131F33"
+                    :secondary "#CCDEFF"
+                    :tertiary "#F7976E"
+                    :quaternary "#FFF8BE"})
