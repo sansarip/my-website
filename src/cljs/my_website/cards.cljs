fc59c4f3752b4f8c01040160d8b308b838d34aa5
sansarip <pehrans@gmail.com>
2019-09-02 19:03:21 -0400

    added more cards

--- src/cljs/my_website/cards.cljs
+++ src/cljs/my_website/cards.cljs
@@ -1,6 +1,8 @@
 (ns my-website.cards
   (:require [devcards.core :as dc]
-            [my-website.cards.components]))
+            [my-website.cards.components]
+            [my-website.cards.home]
+            [my-website.cards.games]))
 
 (enable-console-print!)
 
