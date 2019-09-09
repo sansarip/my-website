5e22cb1e39080c8347246c38e5b8613eae70fb62
sansarip <pehrans@gmail.com>
2019-09-09 10:10:13 -0400

    added properties

--- src/cljs/my_website/components/flexbox.cljs
+++ src/cljs/my_website/components/flexbox.cljs
@@ -2,17 +2,18 @@
   (:require [reagent.core :as r]
             [my-website.utilities :refer [deep-merge word-concat]]
             [my-website.styles :refer [color-palette]]
-            [spade.core :refer [defclass]]
-            [cljs-css-modules.macro :refer-macros [defstyle]]))
+            [spade.core :refer [defclass]]))
 
-(defclass flexbox-class [& {:keys [align justify direction padding width]
+(defclass flexbox-class [& {:keys [align justify direction padding width wrap]
                             :or   {align     "center"
                                    justify   "between"
+                                   wrap      "wrap"
                                    direction "row"
                                    padding   "0"
                                    width     "auto"}}]
           {:display         "flex"
            :flex-wrap       "wrap"
+           :align-content   "space-around"
            :padding-left    padding
            :padding-right   padding
            :width           width
@@ -20,6 +21,7 @@
                                   (= align "end") "flex-end"
                                   :else align)
            :justify-content (cond (= justify "between") "space-between"
+                                  (= justify "around") "space-around"
                                   (= justify "start") "flex-start"
                                   (= justify "end") "flex-end"
                                   :else justify)
@@ -31,7 +33,9 @@
         classes (-> this .-props .-extraClasses)
         align (-> this .-props .-align)
         style (-> this .-props .-style)
+        id (-> this .-props .-id)
         direction (-> this .-props .-direction)
+        wrap (-> this .-props .-wrap)
         padding (-> this .-props .-padding)
         width (-> this .-props .-width)]
     [:div {:class (word-concat
@@ -39,9 +43,11 @@
                                    :direction direction
                                    :align align
                                    :padding padding
-                                   :width width)
+                                   :width width
+                                   :wrap wrap)
                     classes)
            :style style
+           :id    id
            :width width
            :align align}
      children]))
