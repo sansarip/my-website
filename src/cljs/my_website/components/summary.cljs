ef4131a4aac65340f6e0082f625e8a08e9615401
sansarip <pehrans@gmail.com>
2019-09-09 10:10:34 -0400

    changed style type from attr to class

--- src/cljs/my_website/components/summary.cljs
+++ src/cljs/my_website/components/summary.cljs
@@ -3,7 +3,7 @@
             [my-website.utilities :refer [word-concat wrap-all-children deep-merge]]
             [my-website.styles :refer [color-palette font-families font-sizes]]
             [my-website.components.flexbox :refer [flexbox]]
-            [spade.core :refer [defattrs]]
+            [spade.core :refer [defclass]]
             [clojure.spec.alpha :as s]))
 
 (def header-style
@@ -12,28 +12,34 @@
    :font-weight "1000"
    :white-space "nowrap"})
 
-(defattrs summary-class [& {:keys [inverse font-size]
-                            :or   {inverse   false
-                                   font-size nil}}]
+(defclass summary-class [& {:keys [inverse width background]
+                            :or   {inverse false
+                                   width   "auto"
+                                   background "none"}}]
+          {:width   width
+           :display "flex"
+           :cursor "pointer"}
           [".container" {:padding "1em"
+                         :background background
+                         :border-radius "1%"
                          :color   (if inverse "white" (:primary color-palette))}]
           [".separator" {:background-color (:tertiary color-palette)
-                         :width            "7em"
+                         :width            "6.5em"
                          :height           ".5em"
                          :margin-top       "1em"
                          :margin-bottom    "1em"
-                         :transition       "width 2s"}]
-          ["h1" (merge header-style {:font-size (:massive font-sizes)
+                         :transition       "width 1s"}]
+          ["h1" (merge header-style {:font-size  (:massive font-sizes)
                                      :margin-top "1em"})]
-          ["h2" (merge header-style {:font-size (:huge font-sizes)
+          ["h2" (merge header-style {:font-size  (:huge font-sizes)
                                      :margin-top "1em"})]
-          ["h3" (merge header-style {:font-size (:big font-sizes)
+          ["h3" (merge header-style {:font-size  (:big font-sizes)
                                      :margin-top "1em"})]
-          ["h4" (merge header-style {:font-size (:large font-sizes)
+          ["h4" (merge header-style {:font-size  (:large font-sizes)
                                      :margin-top "1em"})]
           [".container:hover > div[name=separator]" {:width "10em"}]
           [".description" {:font-family (:body font-families)
-                           :width       "60%"
+                           :width       (if (not= width "auto") "auto" "60%")
                            :height      "auto"}])
 
 (defn render-fn [this] (let [inverse (-> this .-props .-inverse)
@@ -42,13 +48,23 @@
                              children (-> this .-props .-children)
                              header (-> this .-props .-header)
                              as (-> this .-props .-as)
-                             content (-> this .-props .-content)]
-                         [:div (summary-class
-                                 :inverse inverse)
-                          [:div {:class (word-concat
-                                          "container"
-                                          classes)
-                                 :style style}
+                             content (-> this .-props .-content)
+                             width (-> this .-props .-width)
+                             background (-> this .-props .-background)
+                             background-color (-> this .-props .-backgroundColor)
+                             on-click (-> this .-props .-onClick)]
+                         [:div {:class (summary-class
+                                         :inverse inverse
+                                         :background (if background background "none")
+                                         :width width)
+                                :on-click on-click}
+                          [:> flexbox {:extraClasses (word-concat
+                                                       "container"
+                                                       classes)
+                                       :style        style
+                                       :direction    "column"
+                                       :justify      "start"
+                                       :align "start"}
                            children
                            (wrap-all-children (if as (js->clj as) :h1) header)
                            [:div {:class "separator"
