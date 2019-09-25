f88149a0339ef64473898d5315fdb01d7e260b57
sansarip <pehrans@gmail.com>
2019-09-25 11:59:49 -0400

    Add more component demos

--- src/cljs/my_website/cards/components.cljs
+++ src/cljs/my_website/cards/components.cljs
@@ -7,7 +7,8 @@
             [my-website.components.summary :refer [summary]]
             [my-website.components.menuitem :refer [menuitem]]
             [my-website.components.icon :refer [icon]]
-            [my-website.components.navbar :refer [navbar]]))
+            [my-website.components.navbar :refer [navbar]]
+            [my-website.components.image :refer [image]]))
 
 (defcard
   summary
@@ -70,6 +71,83 @@
                            :inherit-color true
                            :strength      "strong"
                            :size          :huge}]]))))
+
+(defcard
+  image
+  "Basic image component"
+  (fn []
+    (sab/html (as-element
+                [:> image {:src  "https://react.semantic-ui.com/images/wireframe/image.png"
+                           :alt  "Placeholder"
+                           :href "http://www.google.com"
+                           :size :small}]))))
+
+(defcard
+  slide-show-animated-image
+  "Animated image component"
+  (fn []
+    (sab/html (as-element
+                [:> image {:src  ["https://image.flaticon.com/icons/svg/148/148836.svg"
+                                  "https://image.flaticon.com/icons/svg/149/149217.svg"
+                                  "https://image.flaticon.com/icons/svg/149/149760.svg"
+                                  "https://image.flaticon.com/icons/svg/660/660467.svg"]
+                           :alt  "Placeholder"
+                           :href "http://www.google.com"
+                           :size :tiny}]))))
+
+(defcard
+  gif-animated-image
+  "Animated image component"
+  (fn []
+    (sab/html (as-element
+                [:> image {:src    ["https://im4.ezgif.com/tmp/ezgif-4-9b4d3f25ca2e-gif-im/frame_000_delay-0.03s.gif"
+                                    "https://cdn.dribbble.com/users/372537/screenshots/2065624/icons_km_weather.gif"]
+                           :isGif  true
+                           :alt    "Placeholder"
+                           :href   "http://www.google.com"
+                           :width  "40%"
+                           :border (str "5px solid " (:primary color-palette))
+                           :height "40%"}]))))
+
+(defcard
+  slide-show-animated-image-and-summary
+  "Slide show animated image component"
+  (fn []
+    (sab/html (as-element
+                [:> summary {:header   "Animated Hearts"
+                             :width    "20em"
+                             :content  "These hearts don't beat. They animate."
+                             :on-click #(js/alert "Summarize deez Nutellas!")}
+                 [:div {:style {:background-color (:secondary color-palette)
+                                :width            "20em"
+                                :height           "20em"}}
+                  [:> image {:src    ["https://image.flaticon.com/icons/svg/148/148836.svg"
+                                      "https://image.flaticon.com/icons/svg/149/149217.svg"
+                                      "https://image.flaticon.com/icons/svg/149/149760.svg"
+                                      "https://image.flaticon.com/icons/svg/660/660467.svg"]
+                             :alt    "Placeholder"
+                             :width  "20em"
+                             :border (str "5px solid " (:primary color-palette))
+                             :height "20em"}]]]))))
+
+(defcard
+  gif-animated-image-and-summary
+  "Slide show animated image component"
+  (fn []
+    (sab/html (as-element
+                [:> summary {:header   "Weather gif"
+                             :width    "20em"
+                             :content  "Forecast for today is cloudy with a chance of deez Nutellas!"
+                             :on-click #(js/alert "Summarize deez Nutellas!")}
+                 [:> image {:src    ["https://im4.ezgif.com/tmp/ezgif-4-9b4d3f25ca2e-gif-im/frame_000_delay-0.03s.gif"
+                                     "https://cdn.dribbble.com/users/372537/screenshots/2065624/icons_km_weather.gif"]
+                            :alt    "Placeholder"
+                            :isGif  true
+                            :border (str "5px solid " (:primary color-palette))
+                            :width  "100%"
+                            :height "100%"}]]))))
+
+
 
 
 
