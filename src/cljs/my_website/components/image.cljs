89c9ad300cf358860f72d5674cbff7cf5e461d76
sepehr ansaripour <pehrans@gmail.com>
2019-10-06 09:39:13 -0400

    Support additional props

--- src/cljs/my_website/components/image.cljs
+++ src/cljs/my_website/components/image.cljs
@@ -6,22 +6,26 @@
             [spade.core :refer [defclass]]))
 
 (let [default-size "auto"]
-  (defclass image-class [& {:keys [padding size height width clickable border]
-                            :or   {padding   "0"
-                                   size      default-size
-                                   width     default-size
-                                   height    default-size
-                                   clickable false
-                                   border    "none"}}]
+  (defclass image-class [& {:keys [padding size height width clickable border max-width max-height]
+                            :or   {padding    "0"
+                                   size       default-size
+                                   width      default-size
+                                   max-width  default-size
+                                   height     default-size
+                                   max-height default-size
+                                   clickable  false
+                                   border     "none"}}]
             (let [size (cond (string? size) (get sizes (keyword size))
                              (keyword? size) (get sizes size)
                              :else default-size)]
-              {:width     (if (not= width default-size) width size)
-               :height    (if (not= height default-size) height size)
-               :clickable (if clickable
-                            "pointer"
-                            "inherit")
-               :border    border})))
+              {:width      (if (not= width default-size) width size)
+               :max-height max-height
+               :max-width  max-width
+               :height     (if (not= height default-size) height size)
+               :clickable  (if clickable
+                             "pointer"
+                             "inherit")
+               :border     border})))
 
 (defn animate-fn [this]
   (let [current-image-index (.. this -state -currentImageIndex)
@@ -51,7 +55,9 @@
         height (.. this -props -height)
         border (.. this -props -border)
         padding (.. this -props -padding)
-        is-gif (.. this -props -isGif)
+        toggle (.. this -props -toggle)
+        max-width (.. this -props -maxWidth)
+        max-height (.. this -props -maxHeight)
         on-click (.. this -props -onClick)
         clickable (fn? on-click)
         animation-speed (or (.. this -props -animationSpeed) 750)
@@ -62,6 +68,8 @@
                                      (omit-nil-keyword-args
                                        image-class
                                        :padding padding
+                                       :max-width max-width
+                                       :max-height max-height
                                        :width width
                                        :height height
                                        :size size
@@ -71,17 +79,17 @@
                     :src           src
                     :alt           alt
                     :on-mouse-over (cond
-                                     (and multiple-sources (not is-gif))
+                                     (and multiple-sources (not toggle))
                                      (fn [_]
                                        (and (not intervalFn)
                                             (set! (.. this -state -intervalFn)
                                                   (-> js/window
                                                       (.setInterval #(animate-fn this)
                                                                     animation-speed)))))
-                                     (and multiple-sources is-gif)
+                                     (and multiple-sources toggle)
                                      #(animate-fn this))
                     :on-mouse-out  (cond
-                                     (and multiple-sources (not is-gif))
+                                     (and multiple-sources (not toggle))
                                      (fn [_]
                                        (and intervalFn
                                             (do
@@ -89,7 +97,7 @@
                                                     (-> js/window
                                                         (.clearInterval intervalFn)))
                                               (assoc-component-state this -currentImageIndex 0))))
-                                     (and multiple-sources is-gif)
+                                     (and multiple-sources toggle)
                                      #(assoc-component-state this -currentImageIndex 0))
                     :on-click      on-click
                     :style         style}
