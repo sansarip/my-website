fa1446a4f9b19e447e412dff06812bfd24c3070c
sepehr ansaripour <pehrans@gmail.com>
2019-10-14 23:18:23 -0400

    Add new and improved props

--- src/cljs/my_website/components/menuitem.cljs
+++ src/cljs/my_website/components/menuitem.cljs
@@ -4,25 +4,29 @@
             [my-website.styles :refer [color-palette font-families font-sizes]]
             [spade.core :refer [defclass]]))
 
-(defclass menuitem-class [& {:keys [inverse strong font-size padding]
-                             :or   {inverse   false
-                                    strong    false
-                                    font-size :medium
-                                    padding   "0"}}]
+(defclass menuitem-class [& {:keys [inverse strong font-size padding hover-color active-color static color]
+                             :or   {inverse      false
+                                    strong       false
+                                    font-size    :medium
+                                    hover-color  (:tertiary color-palette)
+                                    active-color (:tertiary-alt color-palette)
+                                    static       false
+                                    color        (:primary color-palette)
+                                    padding      "0"}}]
           {:font-family (:body font-families)
            :border      "none"
            :background  "none"
            :transition  "color .25s"
            :padding     padding
-           :color       (if inverse "white" (:primary color-palette))
+           :color       (if inverse "white" color)
            :font-weight (if strong "bold" "normal")
            :text-align  "center"
            :font-size   (cond
                           (string? font-size) ((keyword font-size) font-sizes)
                           (keyword? font-size) (font-size font-sizes))
            :cursor      "pointer"}
-          ["&:hover" {:color (:tertiary color-palette)}]
-          ["&:active" {:color (:tertiary-alt color-palette)}]
+          ["&:hover" {:color (if static color hover-color)}]
+          ["&:active" {:color (if static color active-color)}]
           ["&:focus" {:outline "none"}])
 
 
@@ -31,21 +35,39 @@
         classes (.. this -props -extraClasses)
         style (.. this -props -style)
         strong (.. this -props -strong)
+        href (.. this -props -href)
+        delink (.. this -props -delink)
+        target (or (.. this -props -target) "_blank")
         font-size (.. this -props -fontSize)
+        static (.. this -props -static)
+        color (.. this -props -color)
+        hover-color (.. this -props -hoverColor)
+        active-color (.. this -props -activeColor)
         padding (.. this -props -padding)
         on-click (.. this -props -onClick)
-        inverse (.. this -props -inverse)]
-    [:button {:class     (word-concat
-                           (omit-nil-keyword-args
-                             menuitem-class
-                             :inverse inverse
-                             :strong strong
-                             :padding padding
-                             :font-size font-size)
-                           classes)
-              :on-click  on-click
-              :style     style}
-     children]))
+        inverse (.. this -props -inverse)
+        class (word-concat
+                (omit-nil-keyword-args
+                  menuitem-class
+                  :inverse inverse
+                  :strong strong
+                  :padding padding
+                  :color color
+                  :static static
+                  :hover-color hover-color
+                  :active-color active-color
+                  :font-size font-size)
+                (if delink "delink")
+                classes)]
+    (-> (if href
+          [:a {:class  class
+               :href   href
+               :target target
+               :style  style}]
+          [:button {:class    class
+                    :on-click on-click
+                    :style    style}])
+        (conj children))))
 
 (def menuitem (r/create-class {:display-name :menuitem
                                :render       render-fn}))
