10a66c99ac44b9c06a796f5a19ae440a041dd8d1
sepehr ansaripour <pehrans@gmail.com>
2020-01-13 00:53:28 -0500

    Enhance editor

--- src/cljs/my_website/components/editor.cljs
+++ src/cljs/my_website/components/editor.cljs
@@ -1,10 +1,12 @@
 (ns my-website.components.editor
   (:require
+    [my-website.macros :refer-macros [assoc-component-state]]
     [cljs.pprint :refer [pprint]]
-    [reagent.core :as r]))
     [re-catch.core :as rc]
+    [reagent.core :refer [create-class dom-node with-let]]
     [hljs-kit :refer [Hljs CljHljs] :rename {Hljs        hljs
                                              CljHljs clj-hljs}]))
+
 (defonce _register (doto hljs
                      (.registerLanguage "clojure" clj-hljs)
                      (.registerLanguage "clj" clj-hljs)))
@@ -12,25 +14,42 @@
 (defn editor-did-mount [input]
   (fn [this]
     (let [cm (.fromTextArea js/CodeMirror
-                            (r/dom-node this)
+                            (dom-node this)
                             #js {:mode        "clojure"
-                                 :lineNumbers true})]
-      (.on cm "change" #(reset! input (.getValue %))))))
+                                 :lineNumbers true})
+          on-change (.. this -props -onChange)]
+      (.on cm "change" #(let [val (.getValue %)]
+                          (try
+                            (do (reset! input val)
+                                (if on-change (on-change val)))
+                            (catch js/Error _e)))))))
 
 (defn editor [input]
-  (r/create-class
-    {:render              (fn [] [:textarea
-                                  {:default-value ""
-                                   :auto-complete "off"}])
+  (create-class
+    {:render              (fn [this]
+                            (let [on-change (.. this -props -onChange)]
+                              [:textarea
+                               {:default-value ""
+                                :on-change     on-change
+                                :auto-complete "off"}]))
      :component-did-mount (editor-did-mount input)}))
 
-(defn render-code [this]
-  (->> this r/dom-node (.highlightBlock js/hljs)))
+(defn render-code [_this]
+  (-> js/document
+      (.querySelectorAll "pre code")
       (.forEach #(.highlightBlock hljs %))))
 
 (defn result-view [output]
-  (r/create-class
-    {:render               (fn []
-                             [:pre>code.clj
-                              (with-out-str (pprint @output))])
-     :component-did-update render-code}))                            [rc/catch
+  (create-class
+    {:render              (fn [_this]
+                            [rc/catch
+                             (condp apply [output]
+                               ; render as hiccup if
+                               ;; hiccup vector?
+                               #(and (vector? %) (keyword? (first %))) output
+                               ;; function?
+                               fn? [output]
+                               ; everything else is treated as Clojure code
+                               [:pre>code.clj
+                                (with-out-str (pprint output))])])
+     :component-did-mount render-code}))
