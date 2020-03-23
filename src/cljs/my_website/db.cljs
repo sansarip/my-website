0808c0e6c77a3144e4f3308a577f91f72240077e
sepehr ansaripour <pehrans@gmail.com>
2020-03-23 00:28:34 -0400

    Move default work-items in db to its own location

--- src/cljs/my_website/db.cljs
+++ src/cljs/my_website/db.cljs
@@ -1,4 +1,5 @@
-(ns my-website.db)
+(ns my-website.db
+  (:require [my-website.views.work.defaults :as work-df]))
 
 (def default-db
   {:name     "setootle"
@@ -8,24 +9,5 @@
    :about/db {:state 'start}
    :home/db  {:state 'start}
    :work/db  {:state 'start
-              :work-items [{:names       ["hand-peace" "hand-peace" "hand-peace" "hand-peace"]
-                            :name        :fp
-                            :shelves     [2 25 8 34]
-                            :items       []
-                            :description {}}
-                           {:names       ["poo" "poo" "poo" "poo"]
-                            :name        :containers
-                            :shelves     [2 25 8 34]
-                            :items       []
-                            :description {}}
-                           {:names       ["star" "star" "star" "star"]
-                            :name        :mobile
-                            :shelves     [2 25 8 34]
-                            :items       []
-                            :description {}}
-                           {:names       ["hand-spock" "hand-spock" "hand-spock" "hand-spock"]
-                            :name        :common
-                            :shelves     [2 25 8 34]
-                            :items       []
-                            :description {}}]
+              :work-items work-df/default-work-items
               :work-items-index 0}})
---
+++ src/cljs/my_website/views/work/defaults.cljs
@@ -0,0 +1,22 @@
+(ns my-website.views.work.defaults)
+
+(def default-work-items [{:names       ["hand-peace" "hand-peace" "hand-peace" "hand-peace"]
+                          :name        :fp
+                          :shelves     [2 25 8 34]
+                          :items       []
+                          :description {}}
+                         {:names       ["poo" "poo" "poo" "poo"]
+                          :name        :containers
+                          :shelves     [2 25 8 34]
+                          :items       []
+                          :description {}}
+                         {:names       ["star" "star" "star" "star"]
+                          :name        :mobile
+                          :shelves     [2 25 8 34]
+                          :items       []
+                          :description {}}
+                         {:names       ["hand-spock" "hand-spock" "hand-spock" "hand-spock"]
+                          :name        :common
+                          :shelves     [2 25 8 34]
+                          :items       []
+                          :description {}}])
