ee808ec54eeb0d29f6516bed75ed2ed0dc6800cc
sepehr ansaripour <pehrans@gmail.com>
2020-03-24 09:52:24 -0400

    Begin to implement high-level components file

---
+++ src/cljs/my_website/ui.cljs
@@ -0,0 +1,8 @@
+(ns my-website.ui
+  (:require [my-website.components.icon :as icon-component]
+            [my-website.components.animate :as animate-icon]
+            [my-website.components.menuitem :as menuitem-component]))
+
+(def icon icon-component/icon)
+(def menuitem menuitem-component/menuitem)
+(def animate animate-icon/animate)
