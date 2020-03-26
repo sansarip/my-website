e2533870a3af4a400b85b9a5c62cd2bc194bb697
sepehr ansaripour <pehrans@gmail.com>
2020-03-26 09:08:46 -0400

    Upgrade Spade and change optimization level to simple

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [ring "1.7.1"]
                  [devcards "0.2.6"]
                  [binaryage/devtools "0.9.10"]
-                 [net.dhleong/spade "1.0.3"]
+                 [net.dhleong/spade "1.0.4"]
                  [org.clojure/test.check "0.9.0"]
                  [re-catch "0.1.4"]]
   :min-lein-version "2.5.3"
