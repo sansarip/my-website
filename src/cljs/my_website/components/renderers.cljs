3dd3a87792ea23dcc049422c699dbe5ff3b4028b
sepehr ansaripour <pehrans@gmail.com>
2021-03-10 22:13:54 -0500

    Refactor

--- src/cljs/my_website/components/renderers.cljs
+++ src/cljs/my_website/components/renderers.cljs
@@ -1,16 +1,17 @@
 (ns my-website.components.renderers
   (:require [my-website.components.menuitem :refer [menuitem]]
             [my-website.styles :refer [color-palette]]
-            [reagent.core :refer [as-element]]
+            [reagent.core :as r]
             [my-website.utilities :refer [deep-merge]]))
 
-(defn gen-renderers [& {:keys [link-options]}]
+(defn renderer-options [& [link-opts]]
   {:link (fn [props]
-           (as-element
-             [:> menuitem (deep-merge {:delink      true
-                                       :color       (:tertiary color-palette)
-                                       :hover-color (:tertiary-alt color-palette)
-                                       :href        (.-href props)
-                                       :strong      true}
-                                      link-options)
+           (r/as-element
+             [:> menuitem (r/merge-props
+                            {:delink      true
+                             :color       (:tertiary color-palette)
+                             :hover-color (:tertiary-alt color-palette)
+                             :href        (.-href props)
+                             :strong      true}
+                            link-opts)
               (.-children props)]))})
--- src/cljs/my_website/components/text.cljs
+++ src/cljs/my_website/components/text.cljs
@@ -4,19 +4,19 @@
             [my-website.utilities :refer [word-concat wrap-all-children deep-merge omit-nil-keyword-args]]
             [my-website.styles :refer [header-style color-palette font-families font-sizes]]
             [my-website.components.flexbox :refer [flexbox]]
-            [my-website.components.renderers :refer [gen-renderers]]
+            [my-website.components.renderers :refer [renderer-options]]
             [spade.core :refer [defclass]]
             [clojure.spec.alpha :as s]))
 
 (defclass text-class [& {:keys [inverse]
                          :or   {inverse false}}]
-          {:color (if inverse "white" "inherit")}
-          ["h1" {:color (if inverse "white" "inherit")}]
-          ["h2" {:color (if inverse "white" "inherit")}]
-          ["h3" {:color (if inverse "white" "inherit")}]
-          ["h4" {:color (if inverse "white" "inherit")}]
-          ["h5" {:color (if inverse "white" "inherit")}]
-          ["h6" {:color (if inverse "white" "inherit")}])
+  {:color (if inverse "white" "inherit")}
+  ["h1" {:color (if inverse "white" "inherit")}]
+  ["h2" {:color (if inverse "white" "inherit")}]
+  ["h3" {:color (if inverse "white" "inherit")}]
+  ["h4" {:color (if inverse "white" "inherit")}]
+  ["h5" {:color (if inverse "white" "inherit")}]
+  ["h6" {:color (if inverse "white" "inherit")}])
 
 (defn render-fn [this]
   (let [children (.. this -props -children)
@@ -33,9 +33,10 @@
            :style style
            :id    id
            :name  name}
-     [:> markdown (deep-merge {:source    src
-                               :renderers (deep-merge (gen-renderers) (js->clj renderers :keywordize-keys true))}
-                              (js->clj markdown-options :keywordize-keys true))]
+     [:> markdown (r/merge-props
+                    {:source    src
+                     :renderers (deep-merge (renderer-options) (js->clj renderers :keywordize-keys true))}
+                    (js->clj markdown-options :keywordize-keys true))]
      children]))
 
 
