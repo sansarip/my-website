59be33088b44b46580ba04e688ccf1e25253d250
Sepehr Ansaripour <pehrans@gmail.com>
2021-04-03 23:10:39 -0400

    Fix grid-template-columns prop and refactor

--- src/cljs/my_website/components/grid.cljs
+++ src/cljs/my_website/components/grid.cljs
@@ -6,24 +6,30 @@
             [my-website.macros :refer-macros [assoc-component-state get-component-prop]]))
 
 (defclass grid-class [& {:keys [padding
+                                justify-content
+                                align-items
                                 columns
                                 rows
                                 row-gap
                                 column-gap
                                 areas]
-                         :or   {padding       "1em"
-                                columns       "auto"
-                                rows          "auto"
-                                row-gap       "0"
-                                column-gap    "0"
-                                areas         ""}}]
-          {:display               "grid"
-           :padding               padding
-           :grid-template-columns columns
-           :grid-template-rows    rows
-           :grid-column-gap       column-gap
-           :grid-row-gap          row-gap
-           :grid-template-areas   areas})
+                         :or   {padding         "1em"
+                                justify-content "start"
+                                align-items     "start"
+                                columns         "auto"
+                                rows            "auto"
+                                row-gap         "0px"
+                                column-gap      "0px"
+                                areas           ""}}]
+  {:display               "grid"
+   :padding               padding
+   :justify-content       justify-content
+   :align-items           align-items
+   :grid-template-columns columns
+   :grid-template-rows    rows
+   :grid-column-gap       column-gap
+   :grid-row-gap          row-gap
+   :grid-template-areas   areas})
 
 (defn render-fn [this]
   (let [children (.. this -props -children)
@@ -33,23 +39,27 @@
         rows (.. this -props -gridTemplateRows)
         row-gap (.. this -props -gridRowGap)
         column-gap (.. this -props -gridColumnGap)
+        justify-content (.. this -props -justifyContent)
+        align-items (.. this -props -alignItems)
         areas (.. this -props -gridTemplateAreas)
         id (.. this -props -id)
         name (.. this -props -name)
         padding (.. this -props -padding)]
     [:div {:class (word-concat
-                    (omit-nil-keyword-args grid-class
-                                           :padding padding
-                                           :row-gap row-gap
-                                           :grid-column-gap column-gap
-                                           :columns (if columns (word-concat columns))
-                                           :rows (if rows (word-concat rows))
-                                           :areas (if areas (seq->css-grid-areas areas)))
-
+                    (omit-nil-keyword-args
+                      grid-class
+                      :padding padding
+                      :row-gap row-gap
+                      :column-gap column-gap
+                      :columns (if columns (word-concat columns))
+                      :rows (if rows (word-concat rows))
+                      :areas (if areas (seq->css-grid-areas areas))
+                      :justify-content justify-content
+                      :align-items align-items)
                     classes)
            :style style
-           :id id
-           :name name}
+           :id    id
+           :name  name}
      children]))
 
 (def grid (r/create-class {:display-name :grid
