16741447a2d9f993ddeacace8051e9cf73a5bb83
Sepehr Ansaripour <pehrans@gmail.com>
2021-04-03 23:11:27 -0400

    Add title text next to step and refactor

--- src/cljs/my_website/components/steps.cljs
+++ src/cljs/my_website/components/steps.cljs
@@ -2,45 +2,56 @@
   (:require
     [my-website.components.icon :refer [icon]]
     [my-website.components.grid :refer [grid]]
+    [my-website.components.text :refer [text]]
     [my-website.styles :refer [color-palette font-families font-sizes]]
     [my-website.utilities :refer [word-concat deep-merge wrap-each-child omit-nil-keyword-args]]
     [reagent.core :as r]
     [spade.core :refer [defclass defkeyframes]]))
 
 (defkeyframes glow-frames [inverse]
-              [:to {:text-shadow (str "0 0 7px " (if inverse
+              [:to {:text-shadow (str "0 0 4px " (if inverse
                                                    (:tertiary color-palette)
                                                    (:tertiary-alt color-palette)))}])
 
 (defclass steps-class [inverse]
-          [".selected" {:animation [[(glow-frames inverse) "1.2s" "infinite" "alternate"]]}])
+  [:.selected {:animation [[(glow-frames inverse) "1.2s" "infinite" "alternate"]]}]
+  [:p {:margin :0px}])
 
 (defn render-fn [this]
   (let [inverse (.. this -props -inverse)
         items (.. this -props -items)
         selected-key (.. this -props -selectedKey)
         size (or (.. this -props -size) :tiny)
-        step-gap (or (.. this -props -stepGap) ".4em")
+        step-gap (or (.. this -props -stepGap) ".325em")
         style (.. this -props -style)
         classes (.. this -props -extraClasses)]
-    (reduce-kv (fn [c k v]
-                 (let [given-key (or (:key v) k)
-                       on-click (:on-click v)
-                       selected? (= given-key selected-key)]
-                   (conj c [:> icon {:icon-name     "circle"
-                                     :extra-classes (if selected? "selected")
-                                     :inverse       inverse
-                                     :title         (:title v)
-                                     :on-click      (if on-click #(on-click given-key))
-                                     :strength      (if selected? "strong"
-                                                                  "light")
-                                     :size          size}])))
-               [:> grid {:extra-classes         (word-concat (steps-class inverse) classes)
-                         :style                 style
-                         :grid-template-columns (get font-sizes (keyword size))
-                         :grid-template-rows    "auto"
-                         :grid-row-gap          step-gap}]
-               (js->clj items :keywordize-keys true))))
+    (into
+      [:> grid {:extra-classes         (word-concat (steps-class inverse) classes)
+                :style                 style
+                :grid-template-columns "auto"
+                :grid-template-rows    "auto"
+                :grid-row-gap          step-gap}]
+      (map-indexed
+        (fn [i {k :key on-click :on-click title :title}]
+          (let [given-key (or k i)
+                selected? (= given-key selected-key)
+                selected-class (if selected? "selected")]
+            [:> grid {:grid-column-gap       step-gap
+                      :align-items           "center"
+                      :padding               "0px"
+                      :grid-template-columns "auto auto"}
+             [:> icon {:icon-name     "circle"
+                       :extra-classes selected-class
+                       :inverse       inverse
+                       :title         title
+                       :on-click      (if on-click #(on-click given-key))
+                       :strength      (if selected? "strong" "light")
+                       :size          size}]
+             [:> text {:src           (cond-> title selected? (as-> t (str "**" t "**")))
+                       :extra-classes selected-class
+                       :inverse       true}]])))
+
+      (js->clj items :keywordize-keys true))))
 
 (def steps (r/create-class {:render       render-fn
                             :display-name "steps"}))
